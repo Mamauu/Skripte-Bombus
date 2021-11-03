68c59bb7af1feb9001ed8eeb38bfa4e18e583fdc
Mamauu <ole.felix.nickel@gmail.com>
2021-11-03 15:12:51 +0100

    .

--- vasp/__init__.py
+++ vasp/__init__.py
@@ -8,6 +8,93 @@
 
 
 """
-hier beispiel calc datei
-und file_managment datei
+file_managment.sh
+#works for C/Ne/O/H/Iod/Na systems
+
+i=$1
+zval=$2
+cp CONTCAR POSCAR
+cp HILLSPOT PENALTYPOT
+
+line=2330
+substitute="   POMASS =   20.180; ZVAL   =    $zval    mass and valenz"
+awk -v where="$line" -v what="$substitute" 'FNR==where {print what; next} 1' POTCAR_base > POTCAR
+
+mpirun -machinefile machinefile_$SLURM_JOBID -np $SLURM_NTASKS vasp_gam
+
+#cp XDATCAR data/XDATCAR_$i
+cp OSZICAR data/OSZICAR_$i
+cp OUTCAR  data/OUTCAR_$i
+cp CONTCAR data/CONTCAR_$i #rausnehmen wenn Abstände auch aus XDATCAR_all datei ausgelsen werden können
+#cp LOCPOT  data/LOCPOT_$i  
+#cp AECCAR0 data/AECCAR0_$i
+#cp AECCAR2 data/AECCAR2_$i
+#cp CHGCAR  data/CHGCAR_$i  
+
+/fibus/fs2/04/con4309/Scripts/chgsum.pl AECCAR0 AECCAR2
+/fibus/fs2/04/con4309/Scripts/bader CHGCAR -ref CHGCAR_sum
+cp BCF.dat bader/BCF_$i.dat  
+
+rm CHGCAR_sum AVF.dat BCF.dat
+rm CHG
+
+exit
+
+
+import subprocess
+import numpy as np
+import sys
+sys.path.append('/fibus/fs2/04/con4309/code')
+from vasp import calc
+
+calc.XDATCAR_header() #erzeugt XDATCAR_all datei und liest header aus POSCAR ein
+f = open("2_results.txt", "w") #write header for the results file
+f.write("index zval_ne target Potential Dipol Dipol_dev \n")
+f.close()
+
+#setzt parameter
+zval = 8.000 #startpunkt festlegen
+ramp = False
+target_max = 1.5
+steps = 10
+
+for i in range(1,steps+1):
+	if ramp == True: #setzt target zum maximalen Wert oder erhöht diesen linear
+		target = target_max*(i)/steps
+	else:
+		target = target_max
+
+	#startet die rechnung mit neuem zval
+	subprocess.call(['bash','1_file_managment.sh',str(i),str("{0:0.3f}".format(zval))]) 
+	#rechnet Potential nach dem Rechenschritt aus
+	dipole,dipole_dev = calc.get_dipole_moments_average(["OUTCAR"]) #in diesem Fall nur eine Datei, daher hat die liste nur ein Objekt
+	locpot_dim, atoms = calc.locpot_info()
+	dichte, potential = calc.locpot_potential(locpot_dim,atoms)
+
+	# schreibt die ergebnisse raus
+	calc.XDATCAR_only_pos() #kopiet XDATCAR dateien ohne ihren header zusammen
+	f = open("2_results.txt", "a")
+	f.write(str(i)+" "+str("{0:0.3f}".format(zval))+" "+str("{0:0.3f}".format(target))+" "+str("{0:0.3f}".format(potential))+" "+str("{0:0.5f}".format(dipole[0]))+" "+str("{0:0.5f}".format(dipole_dev[0]))+"\n")
+	f.close()
+
+	#bestimmt wieweit ZVAL abgeändert wird.
+	dif = abs(target - potential)
+	change = round(dif * 10)/1000
+	if change > 0.005:
+		change = 0.005
+
+	if potential < target:
+		zval = zval-change
+	if potential > target:
+		zval = zval+change
 """
+
+
+
+
+
+
+
+
+
+
--- vasp/calc.py
+++ vasp/calc.py
@@ -5,112 +5,125 @@ import math
 
 
 def XDATCAR_header(): 
-    """
-    erzeugt XDATCAR_all datei und liest header aus POSCAR ein
-    Args/Returns:
-	    /
-    """
-    f_XDAT_all = open("XDATCAR_all", "w") 
-    f_header = open("POSCAR","r")
-    all_lines = f_header.readlines()
-    f_XDAT_all.writelines(all_lines[:7])
-    f_header.close()
-    f_XDAT_all.close()
-    return 0
+	"""
+	erzeugt XDATCAR_all datei und liest header aus POSCAR ein
+	Args/Returns:
+		/
+	"""
+	f_XDAT_all = open("XDATCAR_all", "w") 
+	f_header = open("POSCAR","r")
+	all_lines = f_header.readlines()
+	f_XDAT_all.writelines(all_lines[:7])
+	f_header.close()
+	f_XDAT_all.close()
+	return 0
+
+
+def XDATCAR_only_pos(): 
+	"""
+	liest Position aus der XDATCAR datei und schreibt diese in die XDATCAR_all datei 
+	Args/Returns:
+		/
+	"""
+	f_XDAT_all = open("XDATCAR_all", "a") 
+	f_XDAT = open("XDATCAR","r")
+	all_lines = f_XDAT.readlines()
+	f_XDAT_all.writelines(all_lines[7:]) 
+	f_XDAT.close()
+	return 0
 
 
 def locpot_info(filename="LOCPOT"): 
-    """
-    get infos from the LOCPOT file
-    Args:
-        filename (str): /
-    Returns:
-        locpot_dim (list): x,y,z dimensionen der locpot datei
-        atoms (int): number of atoms 
-    """
-    locpot_dim = [0,0,0]
+	"""
+	get infos from the LOCPOT file
+	Args:
+		filename (str): /
+	Returns:
+		locpot_dim (list): x,y,z dimensionen der locpot datei
+		atoms (int): number of atoms 
+	"""
+	locpot_dim = [0,0,0]
 
-    file = open(filename)
-    all_lines = file.readlines()
-    atom_list = all_lines[6].split()
-    atom_list2 = [int(i) for i in atom_list]
-    atoms = sum(atom_list2) 
+	file = open(filename)
+	all_lines = file.readlines()
+	atom_list = all_lines[6].split()
+	atom_list2 = [int(i) for i in atom_list]
+	atoms = sum(atom_list2) 
 
-    locpot_dim[0] = int(all_lines[atoms+9].split()[0])
-    locpot_dim[1] = int(all_lines[atoms+9].split()[1])
-    locpot_dim[2] = int(all_lines[atoms+9].split()[2])
-    print("locpot dimension und atomanzahl",locpot_dim,atoms)
-    return locpot_dim, atoms
+	locpot_dim[0] = int(all_lines[atoms+9].split()[0])
+	locpot_dim[1] = int(all_lines[atoms+9].split()[1])
+	locpot_dim[2] = int(all_lines[atoms+9].split()[2])
+	print("locpot dimension und atomanzahl",locpot_dim,atoms)
+	return locpot_dim, atoms
 
 
 def _clean_line(line):
-    return line.replace("-", " -")
+	return line.replace("-", " -")
 
 
 def _get_lines_from_file(filename, lines=None):
-    """
-    If lines is None read the lines from the file with the filename filename.
-    Args:
-        filename (str): file to read lines from
-        lines (list/ None): list of lines
-
-    Returns:
-        list: list of lines
-    """
-    if lines is None:
-        with open(filename, "r") as f:
-            lines = f.readlines()
-    return lines
+	"""
+	If lines is None read the lines from the file with the filename filename.
+	Args:
+		filename (str): file to read lines from
+		lines (list/ None): list of lines
+	Returns:
+		list: list of lines
+	"""
+	if lines is None:
+		with open(filename, "r") as f:
+			lines = f.readlines()
+	return lines
 
 
 def get_dipole_moments(filename="OUTCAR", lines=None):
-    """
-    Get the average electric dipole moment at every ionic step
-    Args:
-        filename (str): Filename of the OUTCAR file to parse
-        lines (list/None): lines read from the file
-    Returns:
-        list: A list of dipole moments in (eA) for each ionic step
-    """
-    moment_trigger = "dipolmoment"
-    istep_trigger = "FREE ENERGIE OF THE ION-ELECTRON SYSTEM (eV)"
-    dip_moms = list()
-    lines = _get_lines_from_file(filename=filename, lines=lines)
-    istep_mom = list()
-    for i, line in enumerate(lines):
-        line = line.strip()
-        if istep_trigger in line:
-            dip_moms.append(np.average(istep_mom))
-            istep_mom = list()
-        if moment_trigger in line:
-            line = _clean_line(line)
-            mom = np.array([float(val) for val in line.split()[1:4]])
-            istep_mom.append(mom[2])
-    return dip_moms
+	"""
+	Get the average electric dipole moment at every ionic step
+	Args:
+		filename (str): Filename of the OUTCAR file to parse
+		lines (list/None): lines read from the file
+	Returns:
+		list: A list of dipole moments in (eA) for each ionic step
+	"""
+	moment_trigger = "dipolmoment"
+	istep_trigger = "FREE ENERGIE OF THE ION-ELECTRON SYSTEM (eV)"
+	dip_moms = list()
+	lines = _get_lines_from_file(filename=filename, lines=lines)
+	istep_mom = list()
+	for i, line in enumerate(lines):
+		line = line.strip()
+		if istep_trigger in line:
+			dip_moms.append(np.average(istep_mom))
+			istep_mom = list()
+		if moment_trigger in line:
+			line = _clean_line(line)
+			mom = np.array([float(val) for val in line.split()[1:4]])
+			istep_mom.append(mom[2])
+	return dip_moms
 
 
 def get_dipole_moments_average(names):
-    """
-    reads dipole moment for all given filenames
-    Args:
+	"""
+	reads dipole moment for all given filenames
+	Args:
 		names (list): filenames
-    Returns:
-    	dipole_list (list): list with the average dipol for this OUTCAR file
-    """ 
-    dipole_list = []
-    dipole_list_dev=[]
-    for i in names:
-        print("Datei Dipol berechnung",i)
-        file = open(i)
-        dipole_moments = get_dipole_moments(filename=i)
-        dipole_average = np.average(dipole_moments)
-        dipole_dev = np.std(dipole_moments)
-        print("dipole:",dipole_average, dipole_dev)
-        dipole_list.append(dipole_average)
-        dipole_list_dev.append(dipole_dev)
-    return dipole_list, dipole_list_dev
-    
-    
+	Returns:
+		dipole_list (list): list with the average dipol for this OUTCAR file
+	""" 
+	dipole_list = []
+	dipole_list_dev=[]
+	for i in names:
+		print("Datei Dipol berechnung",i)
+		file = open(i)
+		dipole_moments = get_dipole_moments(filename=i)
+		dipole_average = np.average(dipole_moments)
+		dipole_dev = np.std(dipole_moments)
+		print("dipole:",dipole_average, dipole_dev)
+		dipole_list.append(dipole_average)
+		dipole_list_dev.append(dipole_dev)
+	return dipole_list, dipole_list_dev
+	
+	
 def locpot_potential(locpot_dim,atoms,filename="LOCPOT"):
 	"""
 	read Potential from LOCPOT file
@@ -149,14 +162,30 @@ def locpot_potential(locpot_dim,atoms,filename="LOCPOT"):
 		z_dichte = sum(z_dichte2)/x1/x2
 		dichte.append(z_dichte)
 
-	potential = dichte[x3-20]-dichte[5]
+	potential = dichte[x3-16]-dichte[4]
 	print("potential:", potential)
 	return dichte, potential
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+def plot_locpot(dichte,locpot_dim,i=0):
+	x3 = locpot_dim[2]	
+	min = np.min(dichte)-3
+	max = np.max(dichte)+3
+	plt.ylim(min,max)
+	plt.plot(dichte,   c='u')
+	plt.plot([x3-16,-50],[x3-16,50],   c='r')
+	plt.plot([4,-50],[4,50],   c='r')
+	plt.xlabel('calculation step')
+	plt.ylabel('Potential')
+	plt.show()
+	plt.savefig(f"2_locpot_{i}.png", dpi=300)
+	plt.close()
+	return 0
+	
+
+
+
+
+
+
+	
